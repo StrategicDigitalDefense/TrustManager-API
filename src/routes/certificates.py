8dbd94c906f8e2aba3fcf40d6cd9c54cb86971e4
StrategicDigitalDefense <David@StrategicDigitalDefense.com>
2025-09-16 12:45:49 -0500

    started adding basic logging to all subroutines. Just syslog.syslog() but that should do the job. I'll need to setup the Docker container to forward syslog to something like Splunk

--- src/routes/certificates.py
+++ src/routes/certificates.py
@@ -12,11 +12,16 @@ sys.path.append('../models')
 sys.path.append('../db')
 from models.certificates import Certificate
 from db.database import *
+import syslog
+import subprocess
+
+syslog.openlog("TrustManager-API",0,syslog.LOG_LOCAL7)
 
 
 certificates_bp = Blueprint('certificates', __name__)
 
 def parse_certificate(pem_data):
+    syslog.syslog(syslog.LOG_DEBUG,"Calling parse_certificate() with PEM payload\n%s" % (pem_data))
     cert = x509.load_pem_x509_certificate(pem_data.encode(), default_backend())
     subject = cert.subject.rfc4514_string()
     issuer = cert.issuer.rfc4514_string()
@@ -24,6 +29,17 @@ def parse_certificate(pem_data):
     valid_to = cert.not_valid_after
     serial = str(cert.serial_number)
     fingerprint = cert.fingerprint(hashes.SHA256()).hex()
+    syslog.syslog(
+        syslog.LOG_DEBUG,
+        {
+            'subject': subject,
+            'issuer': issuer,
+            'valid_from': valid_from,
+            'valid_to': valid_to,
+            'serial': serial,
+            'fingerprint': fingerprint
+        }
+    )
     return {
         'subject': subject,
         'issuer': issuer,
@@ -37,6 +53,7 @@ def parse_certificate(pem_data):
 def add_certificate():
     #pem_data = request.json.get('pem')
     pem_data = request.data.decode('utf-8') # Read raw data from request body
+    syslog.syslog(syslog.LOG_DEBUG,"Calling add_certificate() with PEM payload:\n%ws" %(pem_data))
     if not pem_data:
         return jsonify({'error': 'PEM data is required'}), 400
 
@@ -65,7 +82,9 @@ def add_certificate():
 
 @certificates_bp.route('/Certificates', methods=['GET'])
 def get_certificates():
+    syslog.syslog(syslog.LOG_DEBUG,"Calling get_certificates()")
     certificates = Certificate.query.all()
+    syslog.syslog(syslog.LOG_DEBUG,"Returning %i responses" % (len(certificates)))
     return [{
         'id': cert.id,
         'subject': cert.subject,
@@ -80,15 +99,19 @@ def get_certificates():
 @certificates_bp.route('/Trust', methods=['POST'])
 def trust_certificate():
     cert_id = request.json.get('id')
+    syslog.syslog(syslog.LOG_DEBUG,"Calling trust_certificate() with certificate ID %i" % (cert_id))
     if not cert_id:
+        syslog.syslog(syslog.LOG_INFO,'Certificate ID is required')
         return jsonify({'error': 'Certificate ID is required'}), 400
 
     certificate = Certificate.query.get(cert_id)
     if not certificate:
+        syslog.syslog(syslog.LOG_INFO,'Certificate not found: id=%i' % cert_id)
         return jsonify({'error': 'Certificate not found'}), 404
 
     # Only allow trusting if subject and issuer match (self-signed)
     if certificate.subject != certificate.issuer:
+        syslog.syslog(syslog.LOG_INFO,'Only self-signed certificates (subject == issuer) can be trusted.')
         return jsonify({'error': 'Only self-signed certificates (subject == issuer) can be trusted.'}), 403
 
     certificate.trusted = True
@@ -101,10 +124,12 @@ def trust_certificate():
 def distrust_certificate():
     cert_id = request.json.get('id')
     if not cert_id:
+        syslog.syslog(syslog.LOG_INFO,'Certificate ID is required')
         return jsonify({'error': 'Certificate ID is required'}), 400
 
     certificate = Certificate.query.get(cert_id)
     if not certificate:
+        syslog.syslog(syslog.LOG_INFO,'Certificate not found: id=%i' % (cert_id))
         return jsonify({'error': 'Certificate not found'}), 404
 
     certificate.trusted = False
@@ -115,26 +140,33 @@ def distrust_certificate():
 
 @certificates_bp.route('/Certificate/serial/<serial>', methods=['GET'])
 def get_certificate_by_serial(serial):
+    syslog.syslog(syslog.LOG_DEBUG,"Calling get_certificate_by_serial() wth serial number %s" % (serial))
     certificate = Certificate.query.filter_by(serial=serial).first()
     if not certificate:
+        syslog.syslog(syslog.LOG_INFO,'Certificate not found: serial=%s' % (serial))
         return jsonify({'error': 'Certificate not found'}), 404
     return certificate.pem, 200
 
 @certificates_bp.route('/Certificate/subject/<subject>', methods=['GET'])
 def get_certificate_by_subject(subject):
+    syslog.syslog(syslog.LOG_DEBUG,"Calling get_certificate_by_subject() with subject filter %s" % (subject))
     certificates = Certificate.query.filter(Certificate.subject.like(f"%{subject}%")).all()
     if not certificates:
+        syslog.syslog(syslog.LOG_INFO,'No certificates found matching subject filter: %s' % (subject))
         return jsonify({'error': 'No certificates found'}), 404
     return [cert.pem for cert in certificates], 200
 
 @certificates_bp.route('/Certificate/fingerprint/<fingerprint>', methods=['GET'])
 def get_certificate_by_fingerprint(fingerprint):
+    syslog.syslog(syslog.LOG_DEBUG,"Calling get_certificate_by_fingerprint() with fingerprint %s" % (fingerprint))
     certificate = Certificate.query.filter_by(fingerprint=fingerprint).first()
     if not certificate:
+        syslog.syslog(syslog.LOG_INFO,'Certificate not found: fingerprint=%s' % (fingerprint))
         return jsonify({'error': 'Certificate not found'}), 404
     return certificate.pem, 200
 
 def generate_atom_feed(certificates):
+    syslog.syslog(syslog.LOG_DEBUG,"Calling generate_atom_feed() with %i certificates" % (len(certificates)))
     updated = max([cert.last_changed for cert in certificates]) if certificates else datetime.utcnow()
     feed = [
         '<?xml version="1.0" encoding="utf-8"?>',
@@ -156,6 +188,7 @@ def generate_atom_feed(certificates):
 
 @certificates_bp.route('/Certificates/atom', methods=['GET'])
 def trusted_certificates_feed():
+    syslog.syslog(syslog.LOG_DEBUG,"Calling trusted_certificates_feed()")
     certificates = Certificate.query.filter_by(trusted=True).all()
     atom_xml = generate_atom_feed(certificates)
     return Response(atom_xml, mimetype='application/atom+xml')
@@ -174,6 +207,8 @@ def get_truststore_file(format):
     Supported formats: jks, pfx, pem, rpm
     """
     filename = TRUSTSTORE_FILES.get(format.lower())
+    syslog.syslog(syslog.LOG_DEBUG,"Calling get_truststore_file() with format %s" % (format))
+    syslog.syslog(syslog.LOG_DEBUG,"Resolved filename: %s" % (filename))
     #if not filename or not os.path.exists(filename):
     #    return jsonify({'error': f'Truststore file for format "{format}" not found.'}), 404
     #return send_file(filename, as_attachment=True)
@@ -198,8 +233,6 @@ BATCH_JOBS = {
     "assemble_group_policy": "batch/assemble_group_policy.py"
 }
 
-import subprocess
-
 @certificates_bp.route('/BatchJob', methods=['POST'])
 def run_batch_job():
     """
@@ -207,7 +240,9 @@ def run_batch_job():
     Request JSON: { "job": "<job_name>" }
     """
     job_name = request.json.get("job")
+    syslog.syslog(syslog.LOG_DEBUG,"Calling run_batch_job() with job name: %s" % (job_name))
     if not job_name or job_name not in BATCH_JOBS:
+        syslog.syslog(syslog.LOG_INFO,'Invalid or missing job name in request.')
         return jsonify({"error": "Invalid or missing job name.", "available_jobs": list(BATCH_JOBS.keys())}), 400
 
     script_path = BATCH_JOBS[job_name]
@@ -233,10 +268,12 @@ def run_batch_job():
 @certificates_bp.route('/BatchJob/list', methods=['GET'])
 def list_batch_jobs():
     # BATCH_JOBS should be defined at module level
+    syslog.syslog(syslog.LOG_DEBUG,"Calling list_batch_jobs()")
     return jsonify(list(BATCH_JOBS.keys()))
 
 @certificates_bp.route('/Truststore/gpo', methods=['GET'])
 def download_latest_gpo_zip():
+    syslog.syslog(syslog.LOG_DEBUG,"Calling download_latest_gpo_zip()")
     gpo_dir = os.path.join('static', 'GPO_Backup')
     zip_files = sorted(
         glob.glob(os.path.join(gpo_dir, '*.zip')),
@@ -244,11 +281,13 @@ def download_latest_gpo_zip():
         reverse=True
     )
     if not zip_files:
+        syslog.syslog(syslog.LOG_INFO,'No GPO backup zip files found.')
         return jsonify({'error': 'No GPO backup zip found.'}), 404
     return send_file(zip_files[0], as_attachment=True)
 
 @certificates_bp.route('/Truststore/gpo/list', methods=['GET'])
 def list_gpo_zips():
+    syslog.syslog(syslog.LOG_DEBUG,"Calling list_gpo_zips()")
     gpo_dir = os.path.join('static', 'GPO_Backup')
     zip_files = sorted(
         glob.glob(os.path.join(gpo_dir, '*.zip')),
@@ -256,12 +295,16 @@ def list_gpo_zips():
         reverse=True
     )
     # Return just the filenames (not full paths)
+    syslog.syslog(syslog.LOG_DEBUG,"Found %i GPO backup zip files" % (len(zip_files)))
+    syslog.syslog(syslog.LOG_DEBUG,"GPO backup zip files: %s" % (zip_files))
     return jsonify([os.path.basename(z) for z in zip_files])
 
 @certificates_bp.route('/Truststore/gpo/<zipname>', methods=['GET'])
 def download_specific_gpo_zip(zipname):
+    syslog.syslog(syslog.LOG_DEBUG,"Calling download_specific_gpo_zip() with zip name: %s" % (zipname))
     gpo_dir = os.path.join('static', 'GPO_Backup')
     zip_path = os.path.join(gpo_dir, zipname)
     if not os.path.exists(zip_path):
+        syslog.syslog(syslog.LOG_INFO,'Requested GPO backup zip not found: %s' % (zipname))
         return jsonify({'error': 'GPO backup zip not found.'}), 404
     return send_file(zip_path, as_attachment=True)
